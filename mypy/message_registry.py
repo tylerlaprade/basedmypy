2a7102d43f6587b2832a42896b30477ac6ce53fd
KotlinIsland <kotlinisland@users.noreply.github.com>
2021-10-22 13:38:57 +1000

    unsafe-variance

--- mypy/message_registry.py
+++ mypy/message_registry.py
@@ -87,7 +87,8 @@ RETURN_TYPE_CANNOT_BE_CONTRAVARIANT: Final = ErrorMessage(
     "Cannot use a contravariant type variable as return type"
 )
 FUNCTION_PARAMETER_CANNOT_BE_COVARIANT: Final = ErrorMessage(
-    "Cannot use a covariant type variable as a parameter"
+    "This usage of this covariant type variable is unsafe as an input parameter.\n"
+    "If this is intentional and you know what you are doing, you can ignore this line with 'unsafe-variance'"
 )
 INCOMPATIBLE_IMPORT_OF: Final = "Incompatible import of"
 FUNCTION_TYPE_EXPECTED: Final = ErrorMessage(
