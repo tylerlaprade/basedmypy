bfbe3f38145329d98e63c5ba7ac54a7e82f4f1e1
KotlinIsland <kotlinisland@users.noreply.github.com>
2021-12-02 23:41:26 +1000

    allow literal types without `Literal`

--- mypy/types.py
+++ mypy/types.py
@@ -766,6 +766,7 @@ class UnboundType(ProperType):
         "empty_tuple_index",
         "original_str_expr",
         "original_str_fallback",
+        "expression",
     )
 
     def __init__(
@@ -778,6 +779,7 @@ class UnboundType(ProperType):
         empty_tuple_index: bool = False,
         original_str_expr: str | None = None,
         original_str_fallback: str | None = None,
+        expression=False,
     ) -> None:
         super().__init__(line, column)
         if not args:
@@ -804,6 +806,9 @@ class UnboundType(ProperType):
         # so we don't have to try and recompute it later
         self.original_str_expr = original_str_expr
         self.original_str_fallback = original_str_fallback
+
+        self.expression = expression
+        """This is used to ban bare Enum literals from expressions like ``TypeAlias``es"""
 
     def copy_modified(self, args: Bogus[Sequence[Type] | None] = _dummy) -> UnboundType:
         if args is _dummy:
@@ -2452,7 +2457,7 @@ class RawExpressionType(ProperType):
         )
     """
 
-    __slots__ = ("literal_value", "base_type_name", "note")
+    __slots__ = ("literal_value", "base_type_name", "note", "expression")
 
     def __init__(
         self,
@@ -2461,11 +2466,13 @@ class RawExpressionType(ProperType):
         line: int = -1,
         column: int = -1,
         note: str | None = None,
+        expression=False,
     ) -> None:
         super().__init__(line, column)
         self.literal_value = literal_value
         self.base_type_name = base_type_name
         self.note = note
+        self.expression = expression
 
     def simple_name(self) -> str:
         return self.base_type_name.replace("builtins.", "")
@@ -2506,15 +2513,21 @@ class LiteralType(ProperType):
     represented as `LiteralType(value="RED", fallback=instance_of_color)'.
     """
 
-    __slots__ = ("value", "fallback", "_hash")
+    __slots__ = ("value", "fallback", "_hash", "bare_literal")
 
     def __init__(
-        self, value: LiteralValue, fallback: Instance, line: int = -1, column: int = -1
+        self,
+        value: LiteralValue,
+        fallback: Instance,
+        line: int = -1,
+        column: int = -1,
+        bare_literal=False,
     ) -> None:
         self.value = value
         super().__init__(line, column)
         self.fallback = fallback
         self._hash = -1  # Cached hash value
+        self.bare_literal = bare_literal
 
     def can_be_false_default(self) -> bool:
         return not self.value
