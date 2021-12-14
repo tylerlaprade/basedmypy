82df2ab9210c515b887c273001f7d274e37e9341
KotlinIsland <kotlinisland@users.noreply.github.com>
2021-12-14 17:17:26 +1000

    set version to 1.1.3

--- mypy/version.py
+++ mypy/version.py
@@ -5,9 +5,9 @@ from mypy import git
 # - Release versions have the form "0.NNN".
 # - Dev versions have the form "0.NNN+dev" (PLUS sign to conform to PEP 440).
 # - For 1.0 we'll switch back to 1.2.3 form.
-__version__ = '2.0.0+dev'
+__version__ = '1.1.3'
 base_version = __version__
-mypy_version = "0.920+dev"
+mypy_version = "0.920 (4d2ff58e)"
 
 mypy_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 if __version__.endswith('+dev') and git.is_git_repo(mypy_dir) and git.have_git():
