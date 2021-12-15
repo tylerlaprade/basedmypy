16edbd2a443a452a798ca3975bf46cdc1fe389d2
KotlinIsland <kotlinisland@users.noreply.github.com>
2021-12-15 18:57:14 +1000

    add legacy flag
    run tests with correct options

--- mypy/main.py
+++ mypy/main.py
@@ -505,13 +505,19 @@ def process_options(args: List[str],
         help="Show program's version number and exit",
         stdout=stdout)
 
-    general_group.add_argument(
+    based_group = parser.add_argument_group(
+        title='Based functionality arguments')
+    based_group.add_argument(
         '--write-baseline', action="store_true",
         help="Create an error baseline from the result of this execution")
-    general_group.add_argument(
+    based_group.add_argument(
         '--baseline-file', action='store',
         help="Use baseline info in the given file"
              "(defaults to '{}')".format(defaults.BASELINE_FILE))
+    based_group.add_argument(
+        '--legacy', action='store_true',
+        help="Disable all based functionality")
+
     config_group = parser.add_argument_group(
         title='Config file',
         description="Use a config file instead of command line arguments. "
@@ -926,6 +932,11 @@ def process_options(args: List[str],
     # This lets `--config-file=` (an empty string) be used to disable all config files.
     if config_file and not os.path.exists(config_file):
         parser.error(f"Cannot find config file '{config_file}'")
+
+    if dummy.legacy:
+        import mypy.options
+
+        mypy.options._based = False
 
     options = Options()
 
