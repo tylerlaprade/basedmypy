4fdef3fb8edfdc4bcdcf3235aa329df6927c16d3
KotlinIsland <kotlinisland@users.noreply.github.com>
2021-12-21 17:29:34 +1000

    remove mypy hash from dev versions

--- mypy/version.py
+++ mypy/version.py
@@ -12,8 +12,6 @@ mypy_version = "0.940+dev"
 mypy_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 if __version__.endswith('+dev') and git.is_git_repo(mypy_dir) and git.have_git():
     __version__ += '.' + git.git_revision(mypy_dir).decode('utf-8')
-    mypy_version += '.' + git.git_revision(mypy_dir).decode('utf-8')
     if git.is_dirty(mypy_dir):
         __version__ += '.dirty'
-        mypy_version += '.dirty'
 del mypy_dir
