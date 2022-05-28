f65a2c6a9e8c12b2b02080f3130faffee8b0e6a4
KotlinIsland <kotlinisland@users.noreply.github.com>
2022-05-28 18:12:15 +1000

    infer property types

--- mypy/nodes.py
+++ mypy/nodes.py
@@ -704,7 +704,8 @@ class FuncItem(FuncBase):
             arg.set_line(self.line, self.column, self.end_line)
 
     def is_dynamic(self) -> bool:
-        return self.type is None
+        from mypy.types import CallableType
+        return self.type is None or isinstance(self.type, CallableType) and self.type.implicit
 
 
 FUNCDEF_FLAGS: Final = FUNCITEM_FLAGS + [
