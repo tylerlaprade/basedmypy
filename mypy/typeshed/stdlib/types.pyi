36091b0f2edc537990fb920547409cc268e79073
KotlinIsland <kotlinisland@users.noreply.github.com>
2022-06-28 20:40:57 +1000

    hack `types.NoneType` to work as a value of `type[None]`
    We will want to revert this when `NoneType` is used as the class of `None`

--- mypy/typeshed/stdlib/types.pyi
+++ mypy/typeshed/stdlib/types.pyi
@@ -17,7 +17,7 @@ from importlib.machinery import ModuleSpec
 
 # pytype crashes if types.MappingProxyType inherits from collections.abc.Mapping instead of typing.Mapping
 from typing import Any, ClassVar, Generic, Mapping, Protocol, TypeVar, overload  # noqa: Y027
-from typing_extensions import Literal, ParamSpec, final
+from typing_extensions import Literal, ParamSpec, final, Final
 
 __all__ = [
     "FunctionType",
@@ -615,9 +615,8 @@ if sys.version_info >= (3, 9):
         def __getattr__(self, name: str) -> Any: ...  # incomplete
 
 if sys.version_info >= (3, 10):
-    @final
-    class NoneType:
-        def __bool__(self) -> Literal[False]: ...
+    # This annotation allows `NoneType` to be used as a value of `type[None]`
+    NoneType: Final[type[None]] = ...
     EllipsisType = ellipsis  # noqa: F821 from builtins
     from builtins import _NotImplementedType
 
