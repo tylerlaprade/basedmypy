0b4e75292e1e78f4c50fce117f693ef5be03ebfb
KotlinIsland <kotlinisland@users.noreply.github.com>
2022-07-13 08:17:02 +1000

    update version to 1.4.0

--- mypy/version.py
+++ mypy/version.py
@@ -14,8 +14,8 @@ based_version_info = VersionInfo(
     1,
     4,
     0,
-    "rc",
-    2,
+    "final",
+    0,
     __version__.split("+dev")[0],
     "dev" if "+dev" in __version__ else "final",
 )
