ba8416c87feba4f36daf16c4ddaaae3b370d57c7
KotlinIsland <kotlinisland@users.noreply.github.com>
2022-07-30 12:29:38 +1000

    support nested TypeVars

--- mypy/checker.py
+++ mypy/checker.py
@@ -6963,6 +6963,7 @@ class CollectArgTypeVarTypes(TypeTraverserVisitor):
         self.arg_types: set[TypeVarType] = set()
 
     def visit_type_var(self, t: TypeVarType) -> None:
+        t.upper_bound.accept(self)
         self.arg_types.add(t)
 
 
