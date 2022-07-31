ac585715b310bc3cdfcc34833f5bfc6405c6337f
KotlinIsland <kotlinisland@users.noreply.github.com>
2022-07-31 16:40:56 +1000

    render "Never" instead of "<nothing>"

--- mypy/messages.py
+++ mypy/messages.py
@@ -2366,6 +2366,8 @@ def format_type_inner(
         if typ.is_noreturn:
             return "NoReturn"
         else:
+            if mypy.options._based:
+                return "Never"
             return "<nothing>"
     elif isinstance(typ, TypeType):
         if not mypy.options._based:
