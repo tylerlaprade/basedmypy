2954b46dd99f70ad35e2a99788fdd4b5e80ee424
KotlinIsland <kotlinisland@users.noreply.github.com>
2022-09-20 23:15:46 +1000

    fix: Set `definition` on `CallableType` where possible

--- mypy/semanal.py
+++ mypy/semanal.py
@@ -860,6 +860,7 @@ class SemanticAnalyzer(
                         defn.arg_names,
                         NoneType(),
                         self.named_type("builtins.function"),
+                        definition=defn,
                         implicit=True,
                     )
             if defn.name == "__new__" and self.options.infer_function_types:
@@ -876,6 +877,7 @@ class SemanticAnalyzer(
                         defn.arg_names,
                         self_type,
                         self.named_type("builtins.function"),
+                        definition=defn,
                         implicit=True,
                     )
             self.prepare_method_signature(defn, self.type, has_self_type)
@@ -6802,6 +6804,7 @@ def infer_fdef_types_from_defaults(defn: FuncDef | Decorator, self: SemanticAnal
                 defn.arg_names,
                 ret_type or UntypedType(),
                 self.named_type("builtins.function"),
+                definition=defn,
                 line=defn.line,
                 column=defn.column,
                 implicit=not self.options.disallow_untyped_defs,
