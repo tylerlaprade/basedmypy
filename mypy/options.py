1663fff04457be21af736b6395b3fba90481d449
KotlinIsland <kotlinisland@users.noreply.github.com>
2022-10-24 23:46:36 +1000

    formatting

--- mypy/options.py
+++ mypy/options.py
@@ -81,6 +81,7 @@ def flip_if_not_based(b: bool) -> bool:
     """
     return b if _based else not b
 
+
 # Features that are currently incomplete/experimental
 TYPE_VAR_TUPLE: Final = "TypeVarTuple"
 UNPACK: Final = "Unpack"
