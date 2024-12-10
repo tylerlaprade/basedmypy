8f3c782a6332f878f775508c5f054ea56e6c64c6
KotlinIsland <kotlinisland@users.noreply.github.com>
2024-12-11 00:06:15 +1000

    update pyprojectx config

--- misc/install.py
+++ misc/install.py
@@ -3,17 +3,13 @@ from subprocess import check_call
 
 
 def main(args: argparse.Namespace):
-    venv_args = ["uv", "venv"]
-    if args.python:
-        venv_args.extend(["--python", args.python, f".venv{args.python}"])
+    venv_args = ["uv", "venv", "--python", args.python, f".venv{args.python}"]
     check_call(venv_args)
-    install_args = ["uv", "pip", "install", "-r", "test-requirements.txt", "-e", "."]
-    if args.python:
-        install_args.extend(["--python", f".venv{args.python}"])
+    install_args = ["uv", "pip", "install", "-r", "test-requirements.txt", "-e", ".", "--python", f".venv{args.python}"]
     check_call(install_args)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--python")
+    parser.add_argument("--python", rquired=True)
     main(parser.parse_args())
