f1a7595ba1a3d38fa0949019d4a00e2cb55f1858
jorenham <jhammudoglu@gmail.com>
2024-12-10 14:58:43 +0100

    `chmod +x` pw

--- misc/install.py
+++ misc/install.py
@@ -5,7 +5,17 @@ from subprocess import check_call
 def main(args: argparse.Namespace):
     venv_args = ["uv", "venv", "--python", args.python, f".venv{args.python}"]
     check_call(venv_args)
-    install_args = ["uv", "pip", "install", "-r", "test-requirements.txt", "-e", ".", "--python", f".venv{args.python}"]
+    install_args = [
+        "uv",
+        "pip",
+        "install",
+        "-r",
+        "test-requirements.txt",
+        "-e",
+        ".",
+        "--python",
+        f".venv{args.python}",
+    ]
     check_call(install_args)
 
 
